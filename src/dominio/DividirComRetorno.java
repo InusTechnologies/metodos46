539c12052f4473625cb891767c7bb230e3a4b50a
92257852915 <artur.santos@serpro.gov.br>
2021-11-25 10:52:36 -0300

    Orientação a objetos dividir com retorno

---
+++ src/dominio/DividirComRetorno.java
@@ -0,0 +1,7 @@
+package dominio;
+
+public class DividirComRetorno {
+    public double DividirComReturn(double num1, double num2){
+        return num1/num2;
+    }
+}
---
+++ src/teste/DividirComRetornoTest.java
@@ -0,0 +1,14 @@
+package teste;
+
+import dominio.DividirComRetorno;
+
+public class DividirComRetornoTest {
+    public static void main(String[] args) {
+
+        DividirComRetorno dividir = new DividirComRetorno();
+        double result = dividir.DividirComReturn(20,5);
+        System.out.println(result);
+
+
+    }
+}
